4be911da9aace61d7dc9159f25cab3bd56aa5c1d
Piyush Maan <psmaan444@gmail.com>
2024-07-26 17:46:01 +0530

    landing update

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const Countdown = () => {
     const calculateTimeLeft = () => {
-        const difference = +new Date('2024-09-28T00:00:00') - +new Date();
+        const difference = +new Date('2024-10-22T00:00:00') - +new Date();
         let timeLeft = {};
 
         if (difference > 0) {
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,10 @@
 const Landing = () => {
     return (
-        <div className='w-screen h-screen sm:min-h-[700px] flex flex-col items-center landing pt-24 sm:pt-32 sm:pt-16  md:pt-8 lg:pt-4 saopaulo z-10'>
-            <h2 className='text-[#95d5b2] md:text-[#005720] font-light md:mb-12 text-2xl'>22-25th October 2024</h2>
+        <div className='w-screen h-screen sm:min-h-[700px] flex flex-col items-center landing pt-24 sm:pt-32 sm:pt-16  md:pt-8 lg:pt-0 saopaulo z-10'>
+            <h2 className='text-[#95d5b2] md:text-[#005720] font-light md:mb-16 text-2xl'>22-25th October 2024</h2>
             <h1 className='font-bold text-6xl sm:text-7xl md:text-8xl text-center w-[90%] text-[#35bc3d] '>Africa <span className='text-white'>Digital Economy Summit</span></h1>
             <h2 className="text-center text-3xl text-[#35bc3d] md:text-[#95d5b2] font-bold mt-2">SAO PAULO EDITION</h2>
-            <h2 className='text-center mt-8 text-2xl text-[#95d5b2] md:text-[#35bc3d] font-["Bebas Neue"] font-semibold w-[80%] md:w-[50%]'>Digital Transformation for Sustainable Growth: Africa’s Path to a Connected Future</h2>
+            <h2 className='text-center mt-8 text-3xl text-[#95d5b2] md:text-white font-["Bebas Neue"] font-semibold w-[80%] md:w-[50%] outliner'>Digital Transformation for Sustainable Growth: Africa’s Path to a Connected Future</h2>
             <h1 className="text-4xl text-white font-bold my-8 text-white md:text-blue-200">A G20 Side Event</h1>
             <div className='flex items-center hero-buttons mt-2 flex-row'>
                 <button className='m-4 font-semibold rounded-lg p-2 px-4 bg-[#35bc3d] text-white'>Brochure</button>
