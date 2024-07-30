d119009b94bcd99a207b438b4773b31988a68314
Piyush Maan <psmaan444@gmail.com>
2024-07-30 14:20:43 +0530

    ticket demo

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -9,10 +9,10 @@ const Tickets = () => {
             <h1 className='text-[#35bc3d] font-bold text-6xl'>Buy Tickets</h1>
             <div className='h-2 w-40 bg-[#35bc3d] rounded-full my-4'></div>
             <div className='flex flex-col md:flex-row items-center mt-8'>
-                <div className='bg-white/20 h-[580px] lg:h-[650px] w-[360px] lg:w-[480px] backdrop-blur-3xl rounded-3xl flex flex-col items-center p-4 py-8 mx-2 lg:mx-4 border-2 border-white/20 hover:scale-105 duration-200 my-4'>
-                    <h1 className='text-white font-bold text-4xl'>Standard</h1>
+                <div className='bg-white/20 h-[580px] lg:h-[650px] w-[360px] lg:w-[300px] xl:w-[400px] backdrop-blur-3xl rounded-3xl flex flex-col items-center p-4 py-8 mx-2 lg:mx-4 border-2 border-white/20 hover:scale-105 duration-200 my-4'>
+                    <h1 className='text-white font-bold text-4xl'>Basic</h1>
                     <img className='w-[90px] my-2' src={ticket}></img>
-                    <h1 className='text-4xl text-[#005720] font-bold'>$500</h1>
+                    <h1 className='text-4xl text-[#005720] font-bold'>$100</h1>
                     <button className='m-4 font-semibold rounded-lg p-2 px-4 bg-[#35bc3d] text-white hover:scale-110 duration-150'>Buy Now</button>
                     <div className='flex flex-col items-start w-[95%]'>
                         <div className='flex items-center border-b-[1px] pb-1 w-full my-1'>
@@ -52,10 +52,53 @@ const Tickets = () => {
                         </div>
                     </div>
                 </div>
-                <div className='bg-white/20 h-[580px] lg:h-[650px] w-[360px] lg:w-[480px] backdrop-blur-3xl rounded-3xl flex flex-col items-center p-4 py-8 mx-2 lg:mx-4 border-2 border-white/20 hover:scale-105 duration-300 my-4'>
+                <div className='bg-white/20 h-[580px] lg:h-[650px] w-[360px] lg:w-[300px] xl:w-[400px] backdrop-blur-3xl rounded-3xl flex flex-col items-center p-4 py-8 mx-2 lg:mx-4 border-2 border-white/20 hover:scale-105 duration-200 my-4'>
+                    <h1 className='text-white font-bold text-4xl'>Standard</h1>
+                    <img className='w-[90px] my-2' src={ticket}></img>
+                    <h1 className='text-4xl text-[#005720] font-bold'>$300</h1>
+                    <button className='m-4 font-semibold rounded-lg p-2 px-4 bg-[#35bc3d] text-white hover:scale-110 duration-150'>Buy Now</button>
+                    <div className='flex flex-col items-start w-[95%]'>
+                        <div className='flex items-center border-b-[1px] pb-1 w-full my-1'>
+                            <div className='w-5 h-5 bg-[#35bc3d] rounded-full mr-3 flex justify-center items-center'>
+                                <img src={tick} className='h-[70%]'></img>
+                            </div>
+                            <h1 className='text-white font-light text-sm lg:text-lg w-[90%]'>⁠Access to all keynote sessions and panel discussions.
+                            </h1>
+                        </div>
+                        <div className='flex items-center border-b-[1px] pb-1 w-full my-1'>
+                            <div className='w-5 h-5 bg-[#35bc3d] rounded-full mr-3 flex justify-center items-center'>
+                                <img src={tick} className='h-[70%]'></img>
+                            </div>
+                            <h1 className='text-white font-light text-sm lg:text-lg w-[90%]'>⁠Entry to the networking zone and exhibition hall.
+                            </h1>
+                        </div>
+                        <div className='flex items-center border-b-[1px] pb-1 w-full my-1'>
+                            <div className='w-5 h-5 bg-[#35bc3d] rounded-full mr-3 flex justify-center items-center'>
+                                <img src={tick} className='h-[70%]'></img>
+                            </div>
+                            <h1 className='text-white font-light text-sm lg:text-lg w-[90%]'>Swag bag with event materials and sponsor information.
+                            </h1>
+                        </div>
+                        <div className='flex items-center border-b-[1px] pb-1 w-full my-1'>
+                            <div className='w-5 h-5 bg-[#35bc3d] rounded-full mr-3 flex justify-center items-center'>
+                                <img src={tick} className='h-[70%]'></img>
+                            </div>
+                            <h1 className='text-white font-light text-sm lg:text-lg'>Coffee breaks and light refreshments.
+                            </h1>
+                        </div>
+                        <div className='flex items-center pb-1 w-full my-1'>
+                            <div className='w-5 h-5 bg-[#35bc3d] rounded-full mr-3 flex justify-center items-center'>
+                                <img src={tick} className='h-[70%]'></img>
+                            </div>
+                            <h1 className='text-white font-light text-sm lg:text-lg w-[90%]'>⁠Invitation to the Africa Business dinner with industry leaders.
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='bg-white/20 h-[580px] lg:h-[650px] w-[360px] lg:w-[300px] xl:w-[400px] backdrop-blur-3xl rounded-3xl flex flex-col items-center p-4 py-8 mx-2 lg:mx-4 border-2 border-white/20 hover:scale-105 duration-300 my-4'>
                     <h1 className='text-white font-bold text-4xl'>Premium</h1>
                     <img className='w-[90px] my-2' src={ticket}></img>
-                    <h1 className='text-4xl text-[#005720] font-bold'>$750</h1>
+                    <h1 className='text-4xl text-[#005720] font-bold'>$500</h1>
                     <button className='m-4 font-semibold rounded-lg p-2 px-4 bg-[#35bc3d] text-white hover:scale-110 duration-150'>Buy Now</button>
                     <div className='flex flex-col items-start w-[95%]'>
                         <div className='flex items-center border-b-[1px] pb-1 w-full my-1'>
